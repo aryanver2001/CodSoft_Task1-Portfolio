7a56fa59ccad82680463870f3278193bcecb7180
ARYAN VERMA <aryanver6@gmail.com>
2024-12-24 12:11:23 +0530

    updated projects

--- src/myComponents/projects.js
+++ src/myComponents/projects.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import SectionDivider from "./Images/divider.png"
-import AIimage from "./Images/project-ai.jpg"
+import Calcimage from "./Images/project-calc.png"
 import Rocketimage from "./Images/project-rocket.png"
 import Shopimage from "./Images/project-shop.png" 
 import Portfolioimage from "./Images/project-portfolio.jpg"
-import { Link } from 'react-router-dom'
+
 
 export default function projects() {
   return (
@@ -18,7 +18,7 @@ export default function projects() {
       <div className='section-parent'>
         <div className='project-head-card'>
           <div className="card project-card" data-aos="fade-out">
-            <img className="card-img-top" src={Shopimage} alt="Card image cap" />
+            <img className="card-img-top" src={Shopimage} />
             <div className="card-body">
               <h4 className="card-title">E-Commerce Website</h4>
               <h6 className="card-subtitle mb-2 text-muted">Tech Used: ReactJs | Bootstrap | HTML | CSS</h6>
@@ -28,7 +28,7 @@ export default function projects() {
             </div>
           </div>
           <div className="card project-card" data-aos="fade-out">
-            <img className="card-img-top" src={Portfolioimage} alt="Card image cap" />
+            <img className="card-img-top" src={Portfolioimage} />
             <div className="card-body">
               <h4 className="card-title">Portfolio Website</h4>
               <h6 className="card-subtitle mb-2 text-muted">Tech Used: ReactJs | Bootstrap | HTML | CSS</h6>
@@ -38,7 +38,7 @@ export default function projects() {
             </div>
           </div>
           <div className="card project-card" data-aos="fade-out">
-            <img className="card-img-top" src={Rocketimage} alt="Card image cap" />
+            <img className="card-img-top" src={Rocketimage} />
             <div className="card-body">
               <h4 className="card-title">RocketMan Website</h4>
               <h6 className="card-subtitle mb-2 text-muted">Tech Used: HTML | CSS</h6>
@@ -48,14 +48,13 @@ export default function projects() {
             </div>
           </div>
           <div className="card project-card" data-aos="fade-out">
-            <img className="card-img-top" src={AIimage} alt="Card image cap" />
+            <img className="card-img-top" src={Calcimage} />
             <div className="card-body">
-              <h4 className="card-title">Brain Tumor Detection AI Model</h4>
-              <h6 className="card-subtitle mb-2 text-muted">Tech Used: TensorFlow | Densenet</h6>
-              <p className="card-text">In collaboration with a team of three members, developed an
-                advanced AI model for precise brain tumor detection using TensorFlow, Densenet, and CNNs. Strengthened tech proficiency,
-                teamwork, and project contributions, elevating overall performance.
+              <h4 className="card-title">Basic Calculator</h4>
+              <h6 className="card-subtitle mb-2 text-muted">Tech Used: ReactJs | Bootstrap | HTML | CSS</h6>
+              <p className="card-text">Developed a basic calculator application using React, demonstrating proficiency in state management, user input handling, and implementing interactive features to enhance functionality and user experience.
               </p>
+              <a href="https://basi-c-alc.netlify.app/" className="btn btn-dark text-white" target='blank'>Explore</a>
             </div>
           </div>
         </div>
